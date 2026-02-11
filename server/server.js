7d03498b8516beade3adcd9267aa4127932d2a9e
Md Safkatul Islam <mdsafkatul.stu24@gmail.com>
2026-02-11 05:33:20 -0800

    initialize Express server with CORS, JSON parsing, and MongoDB connection.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db.js')
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
