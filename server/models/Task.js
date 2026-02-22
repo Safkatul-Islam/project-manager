55059c27c43a741fbf23e9aa6d3608cf95857d4c
Md Safkatul Islam <mdsafkatul.stu24@gmail.com>
2026-02-21 23:53:32 -0800

    feat: Add user reference to task schema for relationship mapping

--- server/models/Task.js
+++ server/models/Task.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
+const User = require('./User');
 
 const taskSchema = mongoose.Schema(
     {
+        // The Relationship
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',  // This tells Mongoose which model this ID belongs to
+            required: true,
+        },
         title: {
             type: String,
             required: [true, 'Please add a text value']
